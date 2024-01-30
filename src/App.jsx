ccd3c5658111716c7167f130570c2420e3fde4e8
rizthewiz <rizthewiz>
2024-01-30 07:43:40 -0500

    working on extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+
 // Write your Color component here
+const Color = ({ color, setSelectedColor }) => {
+  const [current, setCurrent] = useState("");
+  return (
+    <div
+      className={`${color} ${current}`}
+      onClick={() => {
+        setSelectedColor(color);
+        color ? setCurrent("selected") : setCurrent("");
+      }}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="black" setSelectedColor={setSelectedColor} />
+        <Color color="red" setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
